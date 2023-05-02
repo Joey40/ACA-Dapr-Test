dc2190930909a7569bcce7a32f9007d4311a4402
Joe K <jkrilov@gmail.com>
2023-05-02 15:49:47 -0400

    feat: random pass

--- terraform/postgresql.tf
+++ terraform/postgresql.tf
@@ -1,10 +1,27 @@
+resource "random_password" "password" {
+  length           = 30
+  special          = false
+}
+
+resource "azurerm_key_vault_secret" "psql-user" {
+  name         = "psql-user"
+  value        = "dapr_user"
+  key_vault_id = azurerm_key_vault.acadapr.id
+}
+
+resource "azurerm_key_vault_secret" "psql-pass" {
+  name         = "psql-pass"
+  value        = random_password.password.result
+  key_vault_id = azurerm_key_vault.acadapr.id
+}
+
 resource "azurerm_postgresql_flexible_server" "acadapr" {
   name                   = "acadapr-psql"
   resource_group_name    = azurerm_resource_group.baseRG.name
   location               = azurerm_resource_group.baseRG.location
   version                = "14"
-  administrator_login    = "dapr"
-  administrator_password = "wordle"
+  administrator_login    = azurerm_key_vault_secret.psql-user.value
+  administrator_password = random_password.password.result
   zone                   = "2"
 
   storage_mb = 32768
